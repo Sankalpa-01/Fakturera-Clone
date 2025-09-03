3b7095b66481e75f4eb6ae3268fedf1fcc3a4a19
Sankalpa_01 <sankalpaanda@gmail.com>
2025-09-03 16:51:13 +0530

    done changing

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,7 @@ import sequelize from "../config/db.js";
 import Terms from "./terms.model.js";
 import Pricelist from "./pricelist.model.js";
 
-// Sync models
-await sequelize.sync({ alter: true });
+// Sync models - REMOVE THIS LINE
+// await sequelize.sync({ alter: true });
 
 export { sequelize, Terms, Pricelist };
